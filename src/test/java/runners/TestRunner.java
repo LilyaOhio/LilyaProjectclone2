4957f31a62a4640af2e5dd8d6a69e012eb847742
LilyaOhio <lilyacokl@gmail.com>
2024-11-30 13:36:32 -0500

    First

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
     @CucumberOptions( features = "src/test/resources/features/",
     glue="steps",
     dryRun=false,
-    tags="@login",
+    tags="",
     plugin = {"pretty", "html:target/cucumber.html","json:target/cucumber.json",
             // to execute failed test cases one more time
             "rerun:target/failed.tex"}
--- src/test/java/utils/CommonMethods.java
+++ src/test/java/utils/CommonMethods.java
@@ -3,6 +3,7 @@ package utils;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
@@ -28,8 +29,9 @@ public class CommonMethods extends PageInitializer {
     public void openBrowserAndLaunchApplication() throws IOException {
         switch (ConfigReader.read("browser")){
             case "Chrome":
-                //ChromeOptions options = new ChromeOptions();
-                // options.addArguments("--headless");
+                ChromeOptions options = new ChromeOptions();
+                 options.addArguments("--headless");
+                //options.setHeadless(true);
                 driver=new ChromeDriver();
                 break;
             case "FireFox":
